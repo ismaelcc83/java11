7f237ce903ae8b4243e5b223c46325d1e7e711aa
Ismael Castro <ismael.castro@atos.net>
2018-04-23 19:01:17 +0200

    new check

---
+++ src/jeps_314/test.java
@@ -0,0 +1,11 @@
+package jeps_314;
+
+public class test {
+	
+	public static void main(String args[]) throws Exception {
+		try {
+		} catch (Exception e) {
+			System.out.println("Exception in main");
+		}
+	}
+}
