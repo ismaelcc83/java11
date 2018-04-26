9bd034f391048e87f2af20e5554acc749ed0465f
Ismael Castro <ismael.castro@atos.net>
2018-04-26 18:28:31 +0200

    update new test with AllocateHeapAt

--- src/jeps_314/test.java
+++ src/jeps_314/test.java
@@ -2,8 +2,15 @@ package jeps_314;
 
 public class test {
 	
+	// VM Arguments: -XX:AllocateHeapAt=E: -Xms512M -Xmx512M -Duser.language=es -Duser.country=ES -Duser.timezone=Europe/Madrid
 	public static void main(String args[]) throws Exception {
 		try {
+			var i = 1;
+			while (true) {
+				Thread.sleep(5000); // wait 5 seconds
+				System.out.println("Checking heap memory in iteration " + i);
+				++i;
+			}
 		} catch (Exception e) {
 			System.out.println("Exception in main");
 		}
